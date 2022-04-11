86d48b22d1b6c6a3e36a7e774716996d8b5679c8
Joel.Matthewman <JMatthewman@Live.co.uk>
2022-04-11 20:15:34 +0100

    Add support for multi-use codes

--- initialise-database.py
+++ initialise-database.py
@@ -4,4 +4,5 @@ con = sqlite3.connect('roffleBot.db')
 cur = con.cursor()
 
 #cur.execute('DROP TABLE tickets')
-cur.execute('CREATE TABLE tickets (ticket_id INTEGER PRIMARY KEY, code TEXT, source TEXT, created TEXT, claimee INTEGER, claimed TEXT)')
+cur.execute('CREATE TABLE tickets (ticket_id INTEGER PRIMARY KEY, code TEXT, source TEXT, created TEXT, multi_use INTEGER)')
+cur.execute('CREATE TABLE claims (claim_id INTEGER PRIMARY KEY, ticket_id INTEGER, user_id INTEGER, user_name TEXT, claimed TEXT)')
--- roffle-bot.py
+++ roffle-bot.py
@@ -9,25 +9,22 @@ con = sqlite3.connect('roffleBot.db')
 con.row_factory = sqlite3.Row
 cur = con.cursor()
 
-def claimTicket(code, userID):
-  cur.execute('SELECT COUNT(*) AS count FROM tickets WHERE code = ?', [code])
-  ticketCount = cur.fetchone()
-  if ticketCount['count'] == 0:
+def claimTicket(code, user):
+  cur.execute('SELECT *, (SELECT COUNT(*) FROM claims WHERE claims.ticket_id = tickets.ticket_id) AS claims FROM tickets WHERE code = ?', [code])
+  tickets = cur.fetchall()
+  if len(tickets) == 0:
     return f"We couldn't find any tickets matching `{code}`!"
-  elif ticketCount['count'] == 1:
-    cur.execute('SELECT * FROM tickets WHERE code = ?', [code])
-    ticket = cur.fetchone()
-    if ticket['claimee'] == None:
-      cur.execute('UPDATE tickets SET claimee = ?, claimed = CURRENT_TIMESTAMP WHERE ticket_id = ?', [userID, ticket['ticket_id']])
-      con.commit()
+  elif len(tickets) == 1 and (tickets[0]['multi_use'] == 1 or tickets[0]['claims'] == 0):
+    cur.execute('INSERT INTO claims (ticket_id, user_id, user_name, claimed) VALUES (?, ?, ?, CURRENT_TIMESTAMP)', [tickets[0]['ticket_id'], user.id, str(user)])
+    con.commit()
       
-      cur.execute('SELECT COUNT(*) AS count FROM tickets WHERE claimee = ?', [userID])
-      userTickets = cur.fetchone()
-      return f"You've succesfully claimed ticket `{ticket['code']}` from {ticket['source']}. You now have {userTickets['count']} ticket(s) in the raffle! Good luck!"
-    else:
-      return f"That ticket has already been claimed!"
+    cur.execute('SELECT COUNT(*) AS count FROM claims WHERE user_id = ?', [user.id])
+    userTickets = cur.fetchone()
+    return f"You've succesfully claimed ticket `{ticket['code']}` from {ticket['source']}. You now have {userTickets['count']} ticket(s) in the raffle! Good luck!"
+  elif len(tickets) == 1 and tickets[0]['multi_use'] == 0 and tickets[0]['claims'] > 0:
+    return f"That ticket has already been claimed!"
   else:
-    return f"Oh no! We found {ticketCount['count']} tickets matching your code, and that's just not right..."
+    return f"Something went wrong trying to claim your ticket..."
 
 
 import os
@@ -57,7 +54,7 @@ async def create(ctx, count, source):
   codes = []
   for i in range(count):
     newCode = ''.join(random.sample(chars, 6))
-    cur.execute('INSERT INTO tickets (code, source, created) VALUES (?, ?, CURRENT_TIMESTAMP)', [newCode, source])
+    cur.execute('INSERT INTO tickets (code, source, multi_use, created) VALUES (?, ?, 0, CURRENT_TIMESTAMP)', [newCode, source])
     codes.append(newCode)
   con.commit()
   print(type(codes))
@@ -92,9 +89,21 @@ async def create_error(ctx, error):
     raise error
 
 @bot.command()
+@commands.has_role("Roffle Admin")
+async def addMulti(ctx, code, source):
+  print(f"Received request to add multi_use code '{code}' from {ctx.author}")
+  cur.execute('INSERT INTO tickets (code, source, multi_use, created) VALUES (?, ?, 1, CURRENT_TIMESTAMP)', [code, source])
+  con.commit()
+  reply = await ctx.reply("Multi-use code added!")
+  if TIDY:
+    await ctx.message.delete(delay=10)
+      await reply.delete(delay=10)
+
+
+@bot.command()
 async def claim(ctx, code):
   print(f"Received claim request for '{code}' from {ctx.author} ({ctx.author.id})")
-  response = claimTicket(code, ctx.author.id)
+  response = claimTicket(code, ctx.author)
   reply = await ctx.reply(response + ' Self destructing in 10 seconds.')
   print(f"Processed claim request for '{code}' from {ctx.author} ({ctx.author.id})")
   if TIDY:
