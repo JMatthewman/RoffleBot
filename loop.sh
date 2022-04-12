1880be8f20330b3bc789872e3abadba43596d533
Joel.Matthewman <JMatthewman@Live.co.uk>
2022-04-12 08:46:03 +0100

    Add info echos to loop.sh

--- loop.sh
+++ loop.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+echo $(date) " ## Pulling from Git ##"
+echo
 git pull
+echo
+
+echo $(date) " ## Launching roffle-bot.py ##"
+echo
 python roffle-bot.py
+echo $(date) " ## roffle-bot.py exited ##"
+echo
 ./loop.sh
