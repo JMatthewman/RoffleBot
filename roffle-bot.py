0e3d32e21bdae84f007015ae222bad5e6c0e6d7c
LCarpenter87 <L.carpenter@outlook.com>
2022-04-17 23:21:41 +0100

    Update roffle-bot.py

--- roffle-bot.py
+++ roffle-bot.py
@@ -273,6 +273,15 @@ async def listmulti(ctx):
   createMultiList()
   await ctx.reply(multi)
     
+@bot.command()
+@commands.has_any_role(*admin_roles) 
+async def pet_rofflebot(ctx):
+  choices = ['teehehehehehhe', 'ooooohhhhh mooommmyyy', 'me likely', 'sscrrattcchheessss']
+  await ctx.reply(random.choice(choices))
+@pet_rofflebot.error
+async def pet_rofflebot_error(ctx, error):
+  if isinstance(error, commands.MissingRole):
+    await ctx.reply("No touchy the RoffleBot.")
     
 @bot.command()
 @commands.has_any_role(*admin_roles)
